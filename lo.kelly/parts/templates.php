a629bf778231d0c17e81bb09b1c1e8c1f800bbfd
Kelly <kellylo0620@gmail.com>
2022-04-23 22:21:22 -0700

    0423

--- lo.kelly/parts/templates.php
+++ lo.kelly/parts/templates.php
@@ -4,7 +4,7 @@ function productListTemplate($r,$o) {
 return $r.<<<HTML
 <div class="col-xs-12 col-md-4">
      <figure class="figure product">
-        <img src="o->thumbnail" alt="">
+        <img src="img/$o->thumbnail" alt="">
             <figcaption>
                 <div>$o->name</div>
                 <div>&dollar;$o->price</div>
